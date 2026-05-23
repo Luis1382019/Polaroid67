4e8e77b202f8b72264d72d414482eb476064a6a8
Luis1382019 <luisangel1382019.2@hotmail.com>
2026-05-23 12:48:31 -0600

    Prueba

---
+++ Assets/Scripts/Level1/LoopingRoad.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class LoopingRoad : MonoBehaviour
+{
+    [SerializeField] private Transform roadA;
+    [SerializeField] private Transform roadB;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float roadHeight = 10f;
+
+    private bool isRunning = true;
+
+    private void Update()
+    {
+        if (!isRunning) return;
+
+        MoveRoad(roadA);
+        MoveRoad(roadB);
+    }
+
+    private void MoveRoad(Transform road)
+    {
+        road.position += Vector3.down * speed * Time.deltaTime;
+
+        if (road.position.y <= -roadHeight)
+        {
+            road.position += Vector3.up * roadHeight * 2f;
+        }
+    }
+
+    public void StopLoop()
+    {
+        isRunning = false;
+    }
+
+    public void StartLoop()
+    {
+        isRunning = true;
+    }
+}
