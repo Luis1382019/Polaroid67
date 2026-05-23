82c941bdded3214f231d7e7f230473b43237a744
Luis1382019 <luisangel1382019.2@hotmail.com>
2026-05-23 09:16:58 -0600

    Level selector

--- Assets/Scripts/Menu/ChapterSelectSystem.cs
+++ Assets/Scripts/Menu/ChapterSelectSystem.cs
@@ -1,12 +1,57 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class ChapterSelectSystem : MonoBehaviour
 {
-    [SerializeField] private string mainMenuSceneName = "Menu";
-
-    public void GoBackToMainMenu()
+    [System.Serializable]
+    public class ChapterData
     {
-        SceneManager.LoadScene(mainMenuSceneName);
+        public string chapterTitle;
+        public string sceneName;
+        public Sprite backgroundSprite;
+    }
+
+    [Header("Chapters")]
+    [SerializeField] private ChapterData[] chapters;
+    [SerializeField] private int selectedChapterIndex = 0;
+
+    [Header("UI")]
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private TMP_Text chapterTitleText;
+
+    [Header("Scenes")]
+    [SerializeField] private string menuSceneName = "menu";
+
+    private void Start()
+    {
+        SelectChapter(selectedChapterIndex);
+    }
+
+    public void SelectChapter(int chapterIndex)
+    {
+        if (chapterIndex < 0 || chapterIndex >= chapters.Length)
+        {
+            Debug.LogWarning("Índice de capítulo inválido: " + chapterIndex);
+            return;
+        }
+
+        selectedChapterIndex = chapterIndex;
+
+        ChapterData selectedChapter = chapters[selectedChapterIndex];
+
+        backgroundImage.sprite = selectedChapter.backgroundSprite;
+        chapterTitleText.text = selectedChapter.chapterTitle;
+    }
+
+    public void PlaySelectedChapter()
+    {
+        SceneManager.LoadScene(chapters[selectedChapterIndex].sceneName);
+    }
+
+    public void GoBackToMenu()
+    {
+        SceneManager.LoadScene(menuSceneName);
     }
 }
